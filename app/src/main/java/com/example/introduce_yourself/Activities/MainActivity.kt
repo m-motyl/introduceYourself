9b0100bfd4e73a11eec9193e28193bf862f078af
Patryk_Zarzycki_UG <p.zarzycki.501@studms.ug.edu.pl>
2022-11-12 21:31:03 +0100

    Sign in/up login etc. icon; Main Recycler View, user view holder, adapter base;

--- app/src/main/java/com/example/introduce_yourself/Activities/MainActivity.kt
+++ app/src/main/java/com/example/introduce_yourself/Activities/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.introduce_yourself.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.introduce_yourself.R
-import com.example.introduce_yourself.database.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import com.recyclerviewapp.ItemAdapter
+import com.recyclerviewapp.UsersList
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set the LayoutManager that this RecyclerView will use.
+        main_recycler_view.layoutManager = LinearLayoutManager(this)
+
+        // Adapter class is initialized and list is passed in the param.
+        val itemAdapter = UsersList(this, getUsersList())
+
+        // adapter instance is set to the recyclerview to inflate the items.
+        main_recycler_view.adapter = itemAdapter
     }
+
+    private fun getUsersList(): Any {
+
+    }//TODO: Mateusz
 }
---
+++ app/src/main/java/com/example/introduce_yourself/adapters/MainItemAdapter.kt
@@ -0,0 +1,63 @@
+package com.recyclerviewapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.introduce_yourself.R
+import kotlinx.android.synthetic.main.main_item_row.view.*
+
+open class UsersList(
+    private val context: Context,
+    private var listOfUsers: ArrayList<ReadUsermodel> //TODO: Mateusz
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var onClickListener: OnClickListener? = null
+
+    private class OwnViewHolder(
+        view: View
+    ) : RecyclerView.ViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return OwnViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.main_item_row, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val ptr = listOfUsers[position]
+        if (holder is OwnViewHolder) {
+            holder.itemView.main_tv_user_name.text = ptr.login
+            holder.itemView.main_tv_user_description.text = ptr.description //TODO: limit length ~70chars
+            holder.itemView.main_iv_user_picture.setImageBitmap(byteArrayToBitmap(ptr.profile_picture))
+            //passing which position was clicked on rv
+            //passing ptr
+            holder.itemView.setOnClickListener {
+                if (onClickListener != null) {
+                    onClickListener!!.onClick(position, ptr)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listOfUsers.size
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, model: ReadUsermodel)
+    }
+
+    fun byteArrayToBitmap(data: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(data, 0, data.size)
+    }
+}
